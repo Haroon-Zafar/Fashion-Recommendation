a5960d71f79810566b1e9cf3f8ce42845257666e
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-09-16 16:56:58 +0500

    Creating the ResnEt50 model
    Our model is already trained on imagenet, so we will freeze the layers. Using model to predict the image.
    Only adding the top layers

--- app.py
+++ app.py
@@ -3,5 +3,20 @@ from tensorflow.keras.preprocessing.image import image
 from tensorflow.keras.layers import GlobalMaxPooling2D, Dense, Dropout
 from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
 
+# Creating the ResnEt50 model and loading the weights, top layer is false because we will add own ours
+# Standard Image size 224, 224, 3
+
 model = ResNet50(weights='imagenet', include_top=False,
                  input_shape=(224, 224, 3))
+
+# our model is already trained on imagenet, so we will freeze the layers
+# using model to predict the image
+# only adding the top layers
+
+model.trainable = False
+
+# passing our model , and adding the top layers
+
+model = tf.keras.Sequential([model, GlobalMaxPooling2D()])
+
+print(model.summary())
